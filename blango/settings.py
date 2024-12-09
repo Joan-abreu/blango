aa8233e729addecae69d1daba7fb628e597437b6
Joan Abreu <joan.abreu2007@gmail.com>
2024-12-09 19:17:27 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -140,24 +140,31 @@ class Dev(Configuration):
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
   CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-            "formatter": "verbose",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-}
+  LOGGING = {
+			"version": 1,
+			"disable_existing_loggers": False,
+			"formatters": {
+					"verbose": {
+							"format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+							"style": "{",
+					},
+			},
+			"handlers": {
+					"console": {
+							"class": "logging.StreamHandler",
+							"stream": "ext://sys.stdout",
+							"formatter": "verbose",
+					},
+			},
+			"root": {
+					"handlers": ["console"],
+					"level": "DEBUG",
+			},
+	}
+	
+  PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
